56f6c2fa5033ce18c9924348c9c0edbb41829be1
sohanur00 <you@example.com>
2023-07-30 19:20:26 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,29 @@
+// number type variable
+var sunglassprice =120;
+
+// string type variable
+var spacialonename = 'ariana grande';
+
+// boolean type 
+var issingle = true ;
+
+// two type of number :integer:120,
+// float , decimal:3.65
+
+// variable naming convention ;rules, best practice
+/**
+ * Single word
+ * No gap or space
+ * No quotation
+ * no keyword
+ * Cannot start with a number
+ * Cannot contain dash
+ *  should use camelCase
+ 
+
+// Math operation:+, -,*,/
+// shorthand :+=, -=,++, --;
+
+// convery : parse int,Parseploat
+//  tofixed
+*/
